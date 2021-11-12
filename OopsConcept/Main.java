24b40afc279a0071e602468ee1fa628b3864ac7c
Smruti <lenkasmruti123@gmail.com>
2021-11-12 07:14:41 +0530

    Added:P3_ommercialdataprocessing

--- OopsConcept/Main.java
+++ OopsConcept/Main.java
@@ -3,30 +3,44 @@ package OopsConcept;
 import java.util.Scanner;
 
 public class Main {
+	static double totalValue = 0;
+
 	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
-		double totalStock = 0;
+		Company company = new Company();
+		company.setStockSymbol("TATA");
+		company.setNumOfShares(100);
+		company.setShareValue(10000);
+		company.setTotalValueOfShares(company.getNumOfShares() * company.getShareValue());
+		totalValue = totalValue + company.getTotalValueOfShares();
+		StockAccount stock = new StockAccount();
+		stock.buy(company);
 
-		UserInterface userInterface = new UserInterface();
-		StockStore stockStore = new StockStore();
-
+		Scanner scan = new Scanner(System.in);
 		boolean check = true;
 		while (check == true) {
-			System.out.println("Enter 1.Add Stock 2.Display");
-			int choice = sc.nextInt();
+			System.out.println("Enter 1.Buy the Company Shares 2.Sell the Company Shares 3.Display ");
+			int choice = scan.nextInt();
+			scan.nextLine();
 			switch (choice) {
 			case 1:
-				StockValues stock = new StockValues();
-				userInterface.addStock(stock);
-				totalStock = totalStock + stock.getTotalValueOfStock();
-				stockStore.add(stock);
+				Company companies = new Company();
+				stock.addCompany(companies);
+				totalValue = totalValue + companies.getTotalValueOfShares();
+				stock.buy(companies);
 				break;
 			case 2:
-				System.out.println("StockStore");
-				userInterface.print(stockStore.getStockList());
-				System.out.println("Value of Total Stocks is : " + totalStock);
+				Company companyremove = new Company();
+				stock.sell(companyremove);
+				totalValue = totalValue - companyremove.getTotalValueOfShares();
+				break;
+			case 3:
+				System.out.println("StockAccount");
+				stock.printReport(stock.getStockList());
+				System.out.println("TotalValue is :" + totalValue);
+				break;
 			default:
 				check = false;
+				System.out.println("Invalid choice and Exited");
 			}
 		}
 	}
